cb6b647542435c4167aac541bafc6a9b3c1d7a9c
Denis Krestov <rupo@list.ru>
2023-09-28 13:31:44 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const headerBg = document.querySelector("header")
+
+let pictureNumber = 1
+const replaceBg = function () {
+  pictureNumber++
+  headerBg.style.backgroundImage = `url('img/header-bg-large-${pictureNumber}.jpg')`
+  if (pictureNumber=='3'){pictureNumber=0}
+}
+setInterval(replaceBg, 2000)
